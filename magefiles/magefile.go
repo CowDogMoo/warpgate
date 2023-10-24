838548765d4474ba5c572aaee55ee66d1f169674
Jayson Grace <jayson.e.grace@gmail.com>
2023-10-24 00:03:35 -0700

    Refactored `magefiles/magefile.go` to enhance dependency management and compilation process.
    **Added:**
    
    - Introduced `compileParams` struct to manage compilation parameters derived from environment variables or system defaults.
    - Implemented `populateFromEnv` method on `compileParams` struct to fetch or default `GOOS` and `GOARCH` values.
    - Created new `Compile` function to manage project compilation using goreleaser based on environment variables or system defaults.
    - Added error handling for missing or incorrect 'release' environment variable value in `Compile` function.
    - Implemented `doCompile` function within `Compile` to encapsulate the goreleaser command execution logic.
    - Enhanced `InstallDeps` function to run `go mod tidy` on both `magefiles` directory and repo root, including directory navigation and error handling.
    
    **Changed:**
    
    - Modified `InstallDeps` function output messages to reflect the updated logic of running `go mod tidy`.
    - Updated import statements to include necessary packages for the new logic, including `runtime`, `github.com/l50/goutils/v2/dev/mage`, `github.com/l50/goutils/v2/git`, and `github.com/l50/goutils/v2/sys`.
    - Replaced `goutils.Tidy()` calls with `mageutils.Tidy()` in `InstallDeps` function to align with the updated import statements.
    - Re-organized code blocks within `InstallDeps` function to ensure proper sequence of operations and error handling.

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -5,9 +5,13 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
 
 	"github.com/fatih/color"
 	goutils "github.com/l50/goutils"
+	mageutils "github.com/l50/goutils/v2/dev/mage"
+	"github.com/l50/goutils/v2/git"
+	"github.com/l50/goutils/v2/sys"
 
 	// mage utility functions
 	"github.com/magefile/mage/mg"
@@ -20,13 +24,25 @@ func init() {
 
 // InstallDeps Installs go dependencies
 func InstallDeps() error {
-	fmt.Println(color.YellowString("Installing dependencies."))
-
-	if err := goutils.Tidy(); err != nil {
-		return fmt.Errorf(color.RedString(
-			"failed to install dependencies: %v", err))
+	fmt.Println(color.YellowString("Running go mod tidy on magefiles and repo root."))
+	cwd := sys.Gwd()
+	if err := sys.Cd("magefiles"); err != nil {
+		return fmt.Errorf("failed to cd into magefiles directory: %v", err)
 	}
 
+	if err := mageutils.Tidy(); err != nil {
+		return fmt.Errorf("failed to install dependencies: %v", err)
+	}
+
+	if err := sys.Cd(cwd); err != nil {
+		return fmt.Errorf("failed to cd back into repo root: %v", err)
+	}
+
+	if err := mageutils.Tidy(); err != nil {
+		return fmt.Errorf("failed to install dependencies: %v", err)
+	}
+
+	fmt.Println(color.YellowString("Installing dependencies."))
 	if err := goutils.InstallGoPCDeps(); err != nil {
 		return fmt.Errorf(color.RedString(
 			"failed to install pre-commit dependencies: %v", err))
@@ -95,4 +111,103 @@ func UpdateMirror(tag string) error {
 	}
 
 	return nil
+}
+
+type compileParams struct {
+	GOOS   string
+	GOARCH string
+}
+
+func (p *compileParams) populateFromEnv() {
+	if p.GOOS == "" {
+		p.GOOS = os.Getenv("GOOS")
+		if p.GOOS == "" {
+			p.GOOS = runtime.GOOS
+		}
+	}
+
+	if p.GOARCH == "" {
+		p.GOARCH = os.Getenv("GOARCH")
+		if p.GOARCH == "" {
+			p.GOARCH = runtime.GOARCH
+		}
+	}
+}
+
+// Compile compiles the Go project using goreleaser. The behavior is
+// controlled by the 'release' environment variable. If the GOOS and
+// GOARCH environment variables are not set, the function defaults
+// to the current system's OS and architecture.
+//
+// **Environment Variables:**
+//
+// release: Determines the compilation mode.
+//
+// If "true", compiles all supported releases for TTPForge.
+// If "false", compiles only the binary for the specified OS
+// and architecture (based on GOOS and GOARCH) or the current
+// system's default if the vars aren't set.
+//
+// GOOS: Target operating system for compilation. Defaults to the
+// current system's OS if not set.
+//
+// GOARCH: Target architecture for compilation. Defaults to the
+// current system's architecture if not set.
+//
+// Example usage:
+//
+// ```go
+// release=true mage compile # Compiles all supported releases for TTPForge
+// GOOS=darwin GOARCH=arm64 mage compile false # Compiles the binary for darwin/arm64
+// GOOS=linux GOARCH=amd64 mage compile false # Compiles the binary for linux/amd64
+// ```
+//
+// **Returns:**
+//
+// error: An error if any issue occurs during compilation.
+func Compile() error {
+	// Check for the presence of the 'release' environment variable
+	release, ok := os.LookupEnv("release")
+	if !ok {
+		return fmt.Errorf("'release' environment variable not set. It should be 'true' or 'false'. Example: release=true mage Compile")
+	}
+
+	isRelease := false
+	if release == "true" {
+		isRelease = true
+	} else if release != "false" {
+		return fmt.Errorf("invalid value for 'release' environment variable. It should be 'true' or 'false'")
+	}
+
+	if !sys.CmdExists("goreleaser") {
+		return fmt.Errorf("goreleaser is not installed, please run mage installdeps")
+	}
+
+	cwd, err := git.RepoRoot()
+	if err != nil {
+		return err
+	}
+	defer os.Chdir(cwd)
+
+	doCompile := func(release bool) error {
+		var p compileParams
+		p.populateFromEnv() // Populate the GOOS and GOARCH parameters
+
+		var args []string
+
+		if release {
+			fmt.Println("Compiling all supported releases for TTPForge with goreleaser")
+			args = []string{"release", "--snapshot", "--clean", "--skip", "validate"}
+		} else {
+			fmt.Printf("Compiling the TTPForge binary for %s/%s, please wait.\n", p.GOOS, p.GOARCH)
+			args = []string{"build", "--snapshot", "--clean", "--skip", "validate", "--single-target"}
+		}
+
+		if err := sh.RunV("goreleaser", args...); err != nil {
+			return fmt.Errorf("goreleaser failed to execute: %v", err)
+		}
+		return nil
+	}
+
+	return doCompile(isRelease)
 }
