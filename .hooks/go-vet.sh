47dd98e36fe7bcee7c339f7bbb4773e586da1192
Jayson Grace <jayson.e.grace@gmail.com>
2023-10-27 21:48:01 -0700

    Refined GitHub Actions and hooks for better CI/CD.
    **Added:**
    
    - CodeQL Branch Specification: Specified `main` branch for CodeQL analysis on push and PR events.
    - Pre-Commit Trigger Types: Added `opened` and `synchronize` types for Pre-Commit on pull requests.
    - Workflow Dispatch: Enabled manual trigger for Pre-Commit workflow.
    - Go Version Update: Updated Go version to `1.21.0` in Pre-Commit workflow.
    - Renovate Version Update: Updated Renovate action to version `v39.1.0` in Renovate workflow.
    - Semgrep Schedule and Permissions: Added weekly schedule and read permissions for Semgrep Analysis.
    - Generate Docs Hook: Added a new pre-commit hook for generating Go package documentation.
    - Go Licenses Hook: Added a new hook for checking licenses and vendoring dependencies.
    - Go Test Runner Hook: Added a new hook to run Go tests with different configurations.
    
    **Changed:**
    
    - Renamed Actions: Renamed actions in Pre-Commit and Renovate workflows for clarity.
    - Go Vet Script: Simplified package exclusion logic in `go-vet.sh`.
    - Markdown Linting Configuration: Adjusted markdown linting rules in `mdstyle.rb`.
    
    **Removed:**
    
    - YAML Linting Configuration: Removed `.yaml-lint.yaml` as it's no longer needed.

--- .hooks/go-vet.sh
+++ .hooks/go-vet.sh
@@ -5,19 +5,7 @@ pkgs=$(go list ./...)
 
 for pkg in $pkgs; do
     dir="$(basename "$pkg")/"
-    if [[ "${dir}" != ".hooks/" ]] \
-                              && [[ "${dir}" != "bin/" ]] \
-                              && [[ "${dir}" != "cmd/" ]] \
-                              && [[ "${dir}" != "config/" ]] \
-                              && [[ "${dir}" != "deployments/" ]] \
-                              && [[ "${dir}" != "files/" ]] \
-                              && [[ "${dir}" != "images/" ]] \
-                              && [[ "${dir}" != "logs/" ]] \
-                              && [[ "${dir}" != "magefiles/" ]] \
-                              && [[ "${dir}" != "modules/" ]] \
-                              && [[ "${dir}" != "resources/" ]] \
-                              && [[ "${dir}" != "templates/" ]] \
-                              && [[ "${dir}" != "warpgate/" ]]; then
-        go vet "${pkg}/${dir}"
+    if [[ "${dir}" != .*/ ]] && [[ "${dir}" != "magefiles/" ]]; then
+        go vet "${pkg}"
     fi
 done
