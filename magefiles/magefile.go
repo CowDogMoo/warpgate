b962d846bbc25d67637fc4eb1746fd97f7254af4
Jayson Grace <jayson.e.grace@gmail.com>
2023-10-27 21:45:29 -0700

    Created utility functions and documentation for magefiles.
    **Added:**
    
    - Magefiles Folder: Created a new `magefiles` folder to house utility functions and documentation.
    - README Documentation: Added a new `README.md` in the `magefiles` folder, detailing available functions and their usage.
    - Go Tasks: Implemented Go tasks for common operations like compiling, testing, and generating documentation, housed within `magefile.go`.
    - Testing Functions: Introduced a new `testing.go` file within `magefiles` for testing related tasks.
    - Examples and Environment Variables: Included examples and environment variables information in the `README.md`.
    
    **Changed:**
    
    - Mage Targets: Added new mage targets in `magefile.go` with proper documentation to reflect the new utility functions and testing capabilities.
    - Documentation Structure: Updated the documentation structure to be more user-friendly and informative.
    
    **Removed:**
    
    - Old Utility Functions: Removed any redundant or outdated utility functions in preparation for the new magefile structure.

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -1,28 +1,40 @@
 //go:build mage
+// +build mage
 
 package main
 
 import (
 	"fmt"
 	"os"
-	"runtime"
+	"path/filepath"
 
 	"github.com/fatih/color"
-	goutils "github.com/l50/goutils"
+	"github.com/l50/goutils/v2/dev/lint"
 	mageutils "github.com/l50/goutils/v2/dev/mage"
+	"github.com/l50/goutils/v2/docs"
 	"github.com/l50/goutils/v2/git"
 	"github.com/l50/goutils/v2/sys"
-
-	// mage utility functions
-	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
+	"github.com/spf13/afero"
 )
 
 func init() {
 	os.Setenv("GO111MODULE", "on")
 }
 
-// InstallDeps Installs go dependencies
+// InstallDeps installs the Go dependencies necessary for developing
+// on the project.
+//
+// Example usage:
+//
+// ```go
+// mage installdeps
+// ```
+//
+// **Returns:**
+//
+// error: An error if any issue occurs while trying to
+// install the dependencies.
 func InstallDeps() error {
 	fmt.Println(color.YellowString("Running go mod tidy on magefiles and repo root."))
 	cwd := sys.Gwd()
@@ -43,12 +55,11 @@ func InstallDeps() error {
 	}
 
 	fmt.Println(color.YellowString("Installing dependencies."))
-	if err := goutils.InstallGoPCDeps(); err != nil {
-		return fmt.Errorf(color.RedString(
-			"failed to install pre-commit dependencies: %v", err))
+	if err := lint.InstallGoPCDeps(); err != nil {
+		return fmt.Errorf("failed to install pre-commit dependencies: %v", err)
 	}
 
-	if err := goutils.InstallVSCodeModules(); err != nil {
+	if err := mageutils.InstallVSCodeModules(); err != nil {
 		return fmt.Errorf(color.RedString(
 			"failed to install vscode-go modules: %v", err))
 	}
@@ -56,158 +67,109 @@ func InstallDeps() error {
 	return nil
 }
 
-// InstallPreCommitHooks Installs pre-commit hooks locally
-func InstallPreCommitHooks() error {
-	mg.Deps(InstallDeps)
-
-	fmt.Println(color.YellowString("Installing pre-commit hooks."))
-	if err := goutils.InstallPCHooks(); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// RunPreCommit runs all pre-commit hooks locally
-func RunPreCommit() error {
-	mg.Deps(InstallDeps)
-
-	fmt.Println(color.YellowString("Updating pre-commit hooks."))
-	if err := goutils.UpdatePCHooks(); err != nil {
-		return err
-	}
-
-	fmt.Println(color.YellowString(
-		"Clearing the pre-commit cache to ensure we have a fresh start."))
-	if err := goutils.ClearPCCache(); err != nil {
-		return err
-	}
-
-	fmt.Println(color.YellowString("Running all pre-commit hooks locally."))
-	if err := goutils.RunPCHooks(); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// UpdateMirror updates pkg.go.dev with the release associated with the input tag
-func UpdateMirror(tag string) error {
-	var err error
-	fmt.Println(color.YellowString("Updating pkg.go.dev with the new tag %s.", tag))
-
-	err = sh.RunV("curl", "--silent", fmt.Sprintf(
-		"https://sum.golang.org/lookup/github.com/l50/goproject@%s",
-		tag))
-	if err != nil {
-		return fmt.Errorf(color.RedString("failed to update proxy.golang.org: %w", err))
-	}
-
-	err = sh.RunV("curl", "--silent", fmt.Sprintf(
-		"https://proxy.golang.org/github.com/l50/goproject/@v/%s.info",
-		tag))
-	if err != nil {
-		return fmt.Errorf(color.RedString("failed to update pkg.go.dev: %w", err))
-	}
-
-	return nil
-}
-
-type compileParams struct {
-	GOOS   string
-	GOARCH string
-}
-
-func (p *compileParams) populateFromEnv() {
-	if p.GOOS == "" {
-		p.GOOS = os.Getenv("GOOS")
-		if p.GOOS == "" {
-			p.GOOS = runtime.GOOS
-		}
-	}
-
-	if p.GOARCH == "" {
-		p.GOARCH = os.Getenv("GOARCH")
-		if p.GOARCH == "" {
-			p.GOARCH = runtime.GOARCH
-		}
-	}
-}
-
-// Compile compiles the Go project using goreleaser. The behavior is
-// controlled by the 'release' environment variable. If the GOOS and
-// GOARCH environment variables are not set, the function defaults
-// to the current system's OS and architecture.
-//
-// **Environment Variables:**
-//
-// release: Determines the compilation mode.
-//
-// If "true", compiles all supported releases for TTPForge.
-// If "false", compiles only the binary for the specified OS
-// and architecture (based on GOOS and GOARCH) or the current
-// system's default if the vars aren't set.
-//
-// GOOS: Target operating system for compilation. Defaults to the
-// current system's OS if not set.
-//
-// GOARCH: Target architecture for compilation. Defaults to the
-// current system's architecture if not set.
+// GeneratePackageDocs creates documentation for the various packages
+// in the project.
 //
 // Example usage:
 //
 // ```go
-// release=true mage compile # Compiles all supported releases for TTPForge
-// GOOS=darwin GOARCH=arm64 mage compile false # Compiles the binary for darwin/arm64
-// GOOS=linux GOARCH=amd64 mage compile false # Compiles the binary for linux/amd64
+// mage generatepackagedocs
 // ```
 //
 // **Returns:**
 //
-// error: An error if any issue occurs during compilation.
-func Compile() error {
-	// Check for the presence of the 'release' environment variable
-	release, ok := os.LookupEnv("release")
-	if !ok {
-		return fmt.Errorf("'release' environment variable not set. It should be 'true' or 'false'. Example: release=true mage Compile")
-	}
+// error: An error if any issue occurs during documentation generation.
+func GeneratePackageDocs() error {
+	fs := afero.NewOsFs()
 
-	isRelease := false
-	if release == "true" {
-		isRelease = true
-	} else if release != "false" {
-		return fmt.Errorf("invalid value for 'release' environment variable. It should be 'true' or 'false'")
-	}
-
-	if !sys.CmdExists("goreleaser") {
-		return fmt.Errorf("goreleaser is not installed, please run mage installdeps")
-	}
-
-	cwd, err := git.RepoRoot()
+	repoRoot, err := git.RepoRoot()
 	if err != nil {
+		return fmt.Errorf("failed to get repo root: %v", err)
+	}
+	sys.Cd(repoRoot)
+
+	repo := docs.Repo{
+		Owner: "l50",
+		Name:  "goutils/v2",
+	}
+
+	templatePath := filepath.Join("magefiles", "tmpl", "README.md.tmpl")
+	if err := docs.CreatePackageDocs(fs, repo, templatePath); err != nil {
+		return fmt.Errorf("failed to create package docs: %v", err)
+	}
+
+	return nil
+}
+
+// RunPreCommit updates, clears, and executes all pre-commit hooks
+// locally. The function follows a three-step process:
+//
+// First, it updates the pre-commit hooks.
+// Next, it clears the pre-commit cache to ensure a clean environment.
+// Lastly, it executes all pre-commit hooks locally.
+//
+// Example usage:
+//
+// ```go
+// mage runprecommit
+// ```
+//
+// **Returns:**
+//
+// error: An error if any issue occurs at any of the three stages
+// of the process.
+func RunPreCommit() error {
+	fmt.Println("Updating pre-commit hooks.")
+	if err := lint.UpdatePCHooks(); err != nil {
 		return err
 	}
-	defer os.Chdir(cwd)
 
-	doCompile := func(release bool) error {
-		var p compileParams
-		p.populateFromEnv() // Populate the GOOS and GOARCH parameters
-
-		var args []string
-
-		if release {
-			fmt.Println("Compiling all supported releases for TTPForge with goreleaser")
-			args = []string{"release", "--snapshot", "--clean", "--skip", "validate"}
-		} else {
-			fmt.Printf("Compiling the TTPForge binary for %s/%s, please wait.\n", p.GOOS, p.GOARCH)
-			args = []string{"build", "--snapshot", "--clean", "--skip", "validate", "--single-target"}
-		}
-
-		if err := sh.RunV("goreleaser", args...); err != nil {
-			return fmt.Errorf("goreleaser failed to execute: %v", err)
-		}
-		return nil
+	fmt.Println("Clearing the pre-commit cache to ensure we have a fresh start.")
+	if err := lint.ClearPCCache(); err != nil {
+		return err
 	}
 
-	return doCompile(isRelease)
+	fmt.Println("Running all pre-commit hooks locally.")
+	if err := lint.RunPCHooks(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// UpdateMirror updates pkg.go.dev with the release associated with the
+// input tag
+//
+// Example usage:
+//
+// ```go
+// mage updatemirror v2.0.1
+// ```
+//
+// **Parameters:**
+//
+// tag: the tag to update pkg.go.dev with
+//
+// **Returns:**
+//
+// error: An error if any issue occurs while updating pkg.go.dev
+func UpdateMirror(tag string) error {
+	var err error
+	fmt.Printf("Updating pkg.go.dev with the new tag %s.", tag)
+
+	err = sh.RunV("curl", "--silent", fmt.Sprintf(
+		"https://sum.golang.org/lookup/github.com/l50/goutils/v2@%s",
+		tag))
+	if err != nil {
+		return fmt.Errorf("failed to update proxy.golang.org: %w", err)
+	}
+
+	err = sh.RunV("curl", "--silent", fmt.Sprintf(
+		"https://proxy.golang.org/github.com/l50/goutils/v2/@v/%s.info",
+		tag))
+	if err != nil {
+		return fmt.Errorf("failed to update pkg.go.dev: %w", err)
+	}
+
+	return nil
 }
