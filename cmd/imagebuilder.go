f6b044bc4aa278f173c1a95a877500db932421a4
Jayson Grace <jayson.e.grace@gmail.com>
2024-01-24 01:43:19 -0700

    Fixed debug print and accidentally removed var

--- cmd/imagebuilder.go
+++ cmd/imagebuilder.go
@@ -1,25 +1,3 @@
-/*
-Copyright © 2022 Jayson Grace <jayson.e.grace@gmail.com>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
-
 package cmd
 
 import (
@@ -346,12 +324,10 @@ func buildPackerImage(pTmpl PackerTemplate, blueprint Blueprint) error {
 		"-var", fmt.Sprintf("provision_repo_path=%s", blueprint.ProvisioningRepo),
 		"-var", fmt.Sprintf("registry_server=%s", pTmpl.Container.Registry.Server),
 		"-var", fmt.Sprintf("registry_username=%s", pTmpl.Container.Registry.Username),
-		"-var", "registry_cred=******",
+		"-var", fmt.Sprintf("registry_cred=%s", githubToken),
 		"-var", fmt.Sprintf("workdir=%s", pTmpl.Container.Workdir),
 		buildDir,
 	}
-
-	log.L().Println("ARGS: ", args)
 
 	// Change to the build directory
 	if err := os.Chdir(buildDir); err != nil {
