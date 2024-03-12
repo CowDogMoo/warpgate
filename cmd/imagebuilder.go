01a75518d9207da773139c195b9d0212a79ff68d
Jayson Grace <jayson.e.grace@gmail.com>
2024-03-12 00:09:58 -0600

    Broke token validation into registry package

--- cmd/imagebuilder.go
+++ cmd/imagebuilder.go
@@ -2,8 +2,6 @@ package cmd
 
 import (
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
@@ -115,7 +113,7 @@ func validateGitHubToken() error {
 		}
 	}
 
-	if err := validateToken(githubToken); err != nil {
+	if err := registry.ValidateToken(githubToken); err != nil {
 		return fmt.Errorf("GitHub token validation failed: %v", err)
 	}
 
@@ -305,41 +303,6 @@ func initializeBlueprint(blueprintDir string) error {
 		log.L().Errorf(
 			"Failed to initialize blueprint with packer init: %v", err)
 		return err
-	}
-
-	return nil
-}
-
-func validateToken(token string) error {
-	// Define the GitHub API URL for user authentication
-	url := "https://api.github.com/user"
-
-	// Create a new request
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return fmt.Errorf("error creating request: %v", err)
-	}
-
-	// Set the Authorization header with the token
-	req.Header.Set("Authorization", "token "+token)
-
-	// Make the request
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return fmt.Errorf("error making request: %v", err)
-	}
-	defer resp.Body.Close()
-
-	// Read the response body
-	_, err = io.ReadAll(resp.Body)
-	if err != nil {
-		return fmt.Errorf("error reading response: %v", err)
-	}
-
-	// Check if the status code is not 200
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("token validation failed with status code: %d", resp.StatusCode)
 	}
 
 	return nil
