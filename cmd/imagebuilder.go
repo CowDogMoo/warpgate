3228854b1145e3602a9c27a237c561c2a4dd5a32
Jayson Grace <jayson.e.grace@gmail.com>
2024-03-11 23:41:03 -0600

    Refactored monumentally complex func

--- cmd/imagebuilder.go
+++ cmd/imagebuilder.go
@@ -313,117 +313,6 @@ func initializeBlueprint(blueprintDir string) error {
 	return nil
 }
 
-func buildPackerImage(pTmpl *packer.BlueprintPacker, blueprint bp.Blueprint) error {
-	const maxRetries = 3
-	var lastError error
-	var buildDir string
-	var err error
-
-	for attempt := 1; attempt <= maxRetries; attempt++ {
-		buildDir, err = createBuildDir(blueprint)
-		if err != nil {
-			log.L().Errorf(
-				"Failed to create build directory: %v", err)
-			return err
-		}
-
-		if err := initializeBlueprint(buildDir); err != nil {
-			log.L().Errorf(
-				"Failed to initialize blueprint: %v", err)
-			return err
-		}
-
-		if err := viper.UnmarshalKey(containerKey, &pTmpl.Container); err != nil {
-			log.L().Errorf("Failed to unmarshal container parameters "+
-				"from %s config file: %v", blueprint.Name, err)
-			return err
-		}
-
-		templateDir := filepath.Join(buildDir, "packer_templates")
-		log.L().Printf("Packer template path: %s\n", templateDir)
-
-		args := []string{
-			"build",
-			"-var", fmt.Sprintf("base_image=%s", pTmpl.Base.Name),
-			"-var", fmt.Sprintf("base_image_version=%s", pTmpl.Base.Version),
-			"-var", fmt.Sprintf("blueprint_name=%s", pTmpl.Name),
-			"-var", fmt.Sprintf("container_user=%s", pTmpl.Container.User),
-			"-var", fmt.Sprintf("new_image_tag=%s", pTmpl.Tag.Name),
-			"-var", fmt.Sprintf("new_image_version=%s", pTmpl.Tag.Version),
-			"-var", fmt.Sprintf("provision_repo_path=%s", blueprint.ProvisioningRepo),
-			"-var", fmt.Sprintf("registry_server=%s", pTmpl.Container.Registry.Server),
-			"-var", fmt.Sprintf("registry_username=%s", pTmpl.Container.Registry.Username),
-			"-var", fmt.Sprintf("registry_cred=%s", githubToken),
-			"-var", fmt.Sprintf("workdir=%s", pTmpl.Container.Workdir),
-			templateDir,
-		}
-
-		if pTmpl.Container.Entrypoint != "" {
-			args = append(args, "-var", fmt.Sprintf("entrypoint=%s", pTmpl.Container.Entrypoint))
-		}
-
-		// Log the arguments with the token hidden
-		logArgs := make([]string, len(args))
-		copy(logArgs, args)
-		for i, arg := range logArgs {
-			if strings.Contains(arg, "registry_cred=") {
-				logArgs[i] = "-var registry_cred=<HIDDEN>"
-			}
-		}
-		log.L().Printf("Attempt %d - Packer Parameters: %s", attempt, logArgs)
-
-		if err := os.Chdir(buildDir); err != nil {
-			log.L().Errorf(
-				"Failed to change into the %s directory: %v", buildDir, err)
-			return err
-		}
-
-		cmd := sys.Cmd{
-			CmdString: "packer",
-			Args:      args,
-			OutputHandler: func(s string) {
-				log.L().Println(s)
-				// Parse image hashes from the output
-				if strings.Contains(s, "Imported Docker image: sha256:") {
-					parts := strings.Split(s, " ")
-					if len(parts) > 4 {
-						archParts := strings.Split(parts[1], ".")
-						if len(archParts) > 1 {
-							arch := strings.TrimSuffix(archParts[1], ":")
-							for _, part := range parts {
-								if strings.HasPrefix(part, "sha256:") {
-									hash := strings.TrimPrefix(part, "sha256:")
-									if pTmpl.ImageHashes == nil {
-										pTmpl.ImageHashes = make(map[string]string)
-									}
-									pTmpl.ImageHashes[arch] = hash
-									fmt.Printf("DEBUG: Updated ImageHashes: %v\n", pTmpl.ImageHashes) // Debug output
-									break
-								}
-							}
-						}
-					}
-				}
-			},
-		}
-
-		if _, err := cmd.RunCmd(); err != nil {
-			log.L().Errorf(
-				"Attempt %d: Failed to build container image from %s packer template: %s/%v", attempt, buildDir, pTmpl.Name, err)
-			lastError = err
-			continue // retry
-		}
-
-		log.L().Printf("Successfully built container image from the %s packer template as part of the %s blueprint",
-			pTmpl.Name, blueprint.Name)
-
-		return nil // success
-	}
-
-	// If the loop completes, it means all attempts failed
-	return fmt.Errorf("all attempts failed to build container image from %s packer template: %v", pTmpl.Name, lastError)
-}
-
 func validateToken(token string) error {
 	// Define the GitHub API URL for user authentication
 	url := "https://api.github.com/user"
@@ -457,4 +346,126 @@ func validateToken(token string) error {
 	}
 
 	return nil
+}
+
+func buildPackerImage(pTmpl *packer.BlueprintPacker, blueprint bp.Blueprint) error {
+	const maxRetries = 3
+	var lastError error
+
+	for attempt := 1; attempt <= maxRetries; attempt++ {
+		if err := buildImageAttempt(pTmpl, blueprint, attempt); err != nil {
+			lastError = err
+			continue // retry
+		}
+
+		log.L().Printf("Successfully built container image from the %s packer template as part of the %s blueprint",
+			pTmpl.Name, blueprint.Name)
+
+		return nil // success
+	}
+
+	// If the loop completes, it means all attempts failed
+	return fmt.Errorf("all attempts failed to build container image from %s packer template: %v", pTmpl.Name, lastError)
+}
+
+func buildImageAttempt(pTmpl *packer.BlueprintPacker, blueprint bp.Blueprint, attempt int) error {
+	buildDir, err := createBuildDir(blueprint)
+	if err != nil {
+		log.L().Errorf("Failed to create build directory: %v", err)
+		return err
+	}
+
+	if err := initializeBlueprint(buildDir); err != nil {
+		log.L().Errorf("Failed to initialize blueprint: %v", err)
+		return err
+	}
+
+	if err := viper.UnmarshalKey(containerKey, &pTmpl.Container); err != nil {
+		log.L().Errorf("Failed to unmarshal container parameters from %s config file: %v", blueprint.Name, err)
+		return err
+	}
+
+	templateDir := filepath.Join(buildDir, "packer_templates")
+	args := preparePackerArgs(pTmpl, blueprint, templateDir)
+
+	log.L().Printf("Attempt %d - Packer Parameters: %s", attempt, hideSensitiveArgs(args))
+
+	if err := os.Chdir(buildDir); err != nil {
+		log.L().Errorf("Failed to change into the %s directory: %v", buildDir, err)
+		return err
+	}
+
+	return runPackerBuild(args, pTmpl)
+}
+
+func preparePackerArgs(pTmpl *packer.BlueprintPacker, blueprint bp.Blueprint, templateDir string) []string {
+	args := []string{
+		"build",
+		"-var", fmt.Sprintf("base_image=%s", pTmpl.Base.Name),
+		"-var", fmt.Sprintf("base_image_version=%s", pTmpl.Base.Version),
+		"-var", fmt.Sprintf("blueprint_name=%s", pTmpl.Name),
+		"-var", fmt.Sprintf("container_user=%s", pTmpl.Container.User),
+		"-var", fmt.Sprintf("new_image_tag=%s", pTmpl.Tag.Name),
+		"-var", fmt.Sprintf("new_image_version=%s", pTmpl.Tag.Version),
+		"-var", fmt.Sprintf("provision_repo_path=%s", blueprint.ProvisioningRepo),
+		"-var", fmt.Sprintf("registry_server=%s", pTmpl.Container.Registry.Server),
+		"-var", fmt.Sprintf("registry_username=%s", pTmpl.Container.Registry.Username),
+		"-var", fmt.Sprintf("registry_cred=%s", githubToken),
+		"-var", fmt.Sprintf("workdir=%s", pTmpl.Container.Workdir),
+		templateDir,
+	}
+
+	if pTmpl.Container.Entrypoint != "" {
+		args = append(args, "-var", fmt.Sprintf("entrypoint=%s", pTmpl.Container.Entrypoint))
+	}
+
+	return args
+}
+
+func hideSensitiveArgs(args []string) []string {
+	logArgs := make([]string, len(args))
+	copy(logArgs, args)
+	for i, arg := range logArgs {
+		if strings.Contains(arg, "registry_cred=") {
+			logArgs[i] = "-var registry_cred=<HIDDEN>"
+		}
+	}
+	return logArgs
+}
+
+func runPackerBuild(args []string, pTmpl *packer.BlueprintPacker) error {
+	cmd := sys.Cmd{
+		CmdString: "packer",
+		Args:      args,
+		OutputHandler: func(s string) {
+			log.L().Println(s)
+			parseImageHashes(s, pTmpl)
+		},
+	}
+
+	_, err := cmd.RunCmd()
+	return err
+}
+
+func parseImageHashes(output string, pTmpl *packer.BlueprintPacker) {
+	if strings.Contains(output, "Imported Docker image: sha256:") {
+		parts := strings.Split(output, " ")
+		if len(parts) > 4 {
+			archParts := strings.Split(parts[1], ".")
+			if len(archParts) > 1 {
+				arch := strings.TrimSuffix(archParts[1], ":")
+				for _, part := range parts {
+					if strings.HasPrefix(part, "sha256:") {
+						hash := strings.TrimPrefix(part, "sha256:")
+						if pTmpl.ImageHashes == nil {
+							pTmpl.ImageHashes = make(map[string]string)
+						}
+						pTmpl.ImageHashes[arch] = hash
+						fmt.Printf("DEBUG: Updated ImageHashes: %v\n", pTmpl.ImageHashes) // Debug output
+						break
+					}
+				}
+			}
+		}
+	}
 }
